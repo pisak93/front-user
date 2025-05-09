fd726c7aaf99c0f6abdced80923ad1d04f947d57
pisak93 <sebastian.marquez93@hotmail.com>
2025-05-09 00:49:26 -0500

    Ruta del backend

--- app/index.jsx
+++ app/index.jsx
@@ -6,26 +6,39 @@ import styles from "../assets/styles/StylesApp";
 
 function Index() {
 
+
+
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [correo, setCorreo] = useState("");
   const [password, setPassword] = useState("");
-  const [telefono, setTelefono] = useState("");
+  const [telefono, setTelefono] = useState([""]);
   const [activo, setActivo] = useState(true);
+  const [compras, setCompras]= useState({nombreProducto:""});
+
+  
 
   function enviarDatos() {
+
     console.log("enviarDatos");
+
+
 const UsuarioData={
   nombre: nombre,
   apellido: apellido,
   correo: correo,
   password: password,
   telefono: telefono,
-  activo: activo
+  activo: activo,
+  compras:compras
 };
+
+
    axios.post("http://192.168.0.3:5000/registrar",UsuarioData)
+
    .then(function (res){
     console.log(res.data);
+
    
     if(res.data.status){
       alert("Usuario registrado con éxito");
@@ -36,6 +49,7 @@ const UsuarioData={
       alert(res.data.message);
     }
    })
+
    .catch(function (error) {
       console.log(error);
       alert("Error al registrar el usuario");
@@ -46,20 +60,34 @@ const UsuarioData={
 
   return (
     <View style={styles.container}>
+
       <Text style={styles.title} >Registro de usuario</Text>
+
+
       <Text style={styles.text}>Nombre</Text>
       <TextInput  style={styles.input} placeholder="Nombre" value={nombre} onChangeText={setNombre} />
+
+
       <Text style={styles.text}>Apellido</Text>
       <TextInput style={styles.input} placeholder="Apellido" value={apellido} onChangeText={setApellido} />
+
+
       <Text style={styles.text}>Correo</Text>
       <TextInput style={styles.input} placeholder="Email" value={correo} onChangeText={setCorreo} />
+
+
       <Text style={styles.text}>Contraseña</Text>
       <TextInput style={styles.input} placeholder="Contraseña" value={password} onChangeText={setPassword} />
+
+
       <Text style={styles.text}>Telefono</Text>
       <TextInput style={styles.input} placeholder="Teléfono" value={telefono} onChangeText={setTelefono} />
+
+
       <Pressable onPress={enviarDatos} style={styles.button}>
         <Text style={styles.buttonText}>Registrar</Text>
       </Pressable>
+
     </View>
   );
 }
--- app/pedidos.jsx
+++ app/pedidos.jsx
@@ -1,14 +1,16 @@
-import { View,Text,FlatList, ScrollView} from "react-native";
+import { View,Text,FlatList, ScrollView, Pressable} from "react-native";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import TarjetaPedido from "../assets/componentes/TarjetaPedido";
 import styles from "../assets/styles/StylesApp";
+import { TextInput } from "react-native-web";
 
 
 
 function Pedidos(){ 
 
 const[pedidos,setPedidos]=useState({});
+const [fecha, setFecha] = useState("");
 
 
 useEffect(function(){
@@ -16,15 +18,34 @@ useEffect(function(){
    },[]);
 
   function obtenerPedidos(){
-    const pedidosData= axios.get("http://192.168.0.3:5000/assets/pedidos-completos")
+    const pedidosData= axios.get("https://clase-back.onrender.com/assets/pedidos-completos")
     .then(function(res){
         console.log(res.data);
         if(res.data.status){
           setPedidos(res.data.data);
         }
+    })
+    .catch(function(error){
+        console.log(error);
+        alert("Error al obtener los pedidos");
     });
   }
 
+  function consultarPorFecha(){
+   
+
+    const pedidosData= axios.get("https://clase-back.onrender.com/assets/pedidos/solo-platos",{params:{fecha:fecha}})
+    .then(function(res){
+       console.log(res.data);
+        if(res.data.status){
+          setPedidos(res.data.data);
+        }
+    }).catch(function(error){
+        console.log(error);
+        alert("Error al obtener los pedidos");
+    });
+  }
+  
    console.log(pedidos);
 const item= function({item}){
     return(
@@ -33,7 +54,12 @@ const item= function({item}){
 
     return(
         <View style={{flex:1}}>
-            <Text>Pedidos</Text>
+          <Text>Ingrese el año</Text>
+          <TextInput placeholder="EJ:2005" value={fecha} onChangeText={setFecha} />
+        <Pressable onPress={consultarPorFecha}>
+            <Text>Filtrar</Text>
+        </Pressable>
+            <Text>Pedidos de {fecha}</Text>
             <ScrollView>
             <FlatList
             data={pedidos}
